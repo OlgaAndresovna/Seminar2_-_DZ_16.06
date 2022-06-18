0dd09e2d148fadf9b5a7a694a9e3563cbb8a5662
Olga <olg200919@ya.ru>
2022-06-18 12:13:16 +0300

    Д/З задача №16 семинар от 16.06

---
+++ taks16_DZ/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 16: Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+Console.WriteLine("Введите два числа: ");
+int number1 = Convert.ToInt32(Console.ReadLine( ));
+int number2 = Convert.ToInt32(Console.ReadLine( ));
+if (number1 == Math.Sqrt(number2)) 
+{   
+    Console.WriteLine("Да");
+}
+else
+{
+     Console.WriteLine("Нет");
+}
