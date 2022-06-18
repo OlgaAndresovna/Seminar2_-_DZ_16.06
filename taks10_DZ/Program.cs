ed867fd5b60a5ee0f15a76fe0a0eae83ca9fef70
Olga <olg200919@ya.ru>
2022-06-18 11:09:13 +0300

    Д/З задача №10 семинар от 16.06

---
+++ taks10_DZ/Program.cs
@@ -0,0 +1,6 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine ("Введите трехзначное число:");
+int number = Convert.ToInt32 (Console.ReadLine());
+int digit1 = number / 10 ;
+int digit2 = digit1 % 10 ;
+Console.WriteLine (digit2);
